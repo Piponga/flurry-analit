de412a5a66bf7668b776b01f9a2667b164e810a7
Piponga <djz@yandex.ru>
2017-10-03 23:59:19 +0300

    Update FlurryAnalyticsPlugin.h

--- src/ios/FlurryAnalyticsPlugin.h
+++ src/ios/FlurryAnalyticsPlugin.h
@@ -1,23 +1,14 @@
-#import <Foundation/Foundation.h>
-#import <Cordova/CDV.h>
+//
+//  FlurryPlugin.h
+//
 
-@interface FlurryAnalyticsPlugin : CDVPlugin
-- (void)initialize:(CDVInvokedUrlCommand *)command;
+#import <Cordova/CDVPlugin.h>
+#import <Cordova/CDVPluginResult.h>
 
-- (void)setUserId:(CDVInvokedUrlCommand *)command;
+@interface FlurryPlugin : CDVPlugin
 
-- (void)setAge:(CDVInvokedUrlCommand *)command;
-
-- (void)setGender:(CDVInvokedUrlCommand *)command;
-
-- (void)logEvent:(CDVInvokedUrlCommand *)command;
-
-- (void)endTimedEvent:(CDVInvokedUrlCommand *)command;
-
-- (void)logPageView:(CDVInvokedUrlCommand *)command;
-
-- (void)logError:(CDVInvokedUrlCommand *)command;
-
-- (void)setLocation:(CDVInvokedUrlCommand *)command;
+- (void)logEvent:(CDVInvokedUrlCommand*)command;
+- (void)logScreenView:(CDVInvokedUrlCommand*)command;
+- (void)endTimedEvent:(CDVInvokedUrlCommand*)command;
 
 @end
